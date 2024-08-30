5d5dbd7aaaa1a2178eb6c69880a73a328ff8d6ce
pandeyaditya934 <pandeyaditya934@gmail.com>
2024-08-29 22:53:30 -0700

    Added CustomerTest

---
+++ src/test/java/actitimeautomation1/CustomerTest.java
@@ -0,0 +1,13 @@
+package actitimeautomation1;
+
+import actitimeautomation.imp1.common.BaseClass;
+import org.testng.annotations.Test;
+
+public class CustomerTest  extends BaseClass {
+
+    @Test
+    public void test(){
+        System.out.println("Hello");
+    }
+
+}
